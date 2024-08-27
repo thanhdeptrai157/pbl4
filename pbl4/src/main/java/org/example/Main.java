010cbb5b2a84c83033f6c4d91e722f30cc78416e
NgKien94 <quangkienofqn@gmail.com>
2024-08-27 16:00:20 +0700

    First commit by Kien

--- pbl4/src/main/java/org/example/Main.java
+++ pbl4/src/main/java/org/example/Main.java
@@ -14,6 +14,8 @@ public class Main {
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
+
+            System.out.println("Xin loi anh Thanh");
         }
     }
 }
