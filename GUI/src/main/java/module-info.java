3e7ef55e35d865c992e551ea35b29c849526f739
Thanh <thanhcap3@gmail.com>
2024-09-14 11:11:46 +0700

    fix ui

---
+++ GUI/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens com.example.gui to javafx.fxml;
+    exports com.example.gui;
+}
